aea5628dff0acc23112d67c755f2f972a7b380b3
Nupur Dave <nupurdave89@gmail.com>
2020-06-07 02:28:31 -0500

    Updates

---
+++ precache-manifest.b3aa673fc5b2d0deab3805058ab701de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488b469d44aae1fc8868c0499fbe8490",
+    "url": "/onlineshopping/index.html"
+  },
+  {
+    "revision": "767093183c6dc8c88f73",
+    "url": "/onlineshopping/static/css/main.7e7b8fae.chunk.css"
+  },
+  {
+    "revision": "832770a87bbe20233307",
+    "url": "/onlineshopping/static/js/2.b5110ea0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/onlineshopping/static/js/2.b5110ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767093183c6dc8c88f73",
+    "url": "/onlineshopping/static/js/main.8a6c271c.chunk.js"
+  },
+  {
+    "revision": "7c3863549905930b2bc7",
+    "url": "/onlineshopping/static/js/runtime-main.da8f695c.js"
+  }
+]);
